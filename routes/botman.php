8cfdbce6115151680c6fc75d13f815d225e6a98f
Vladimir ROmanichev <pro100print@yahoo.com>
2021-01-30 18:07:30 +0300

    add main menu

--- routes/botman.php
+++ routes/botman.php
@@ -2,43 +2,58 @@
 use App\Http\Controllers\BotManController;
 use BotMan\BotMan\BotMan;
 use App\Http\Controllers\VetmanagerController;
+use App\Vetmanager\MainMenu;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
 
 $botman = resolve('botman');
 
+$botman->hears('start', function($bot){
+    $bot->reply(
+        (
+            new MainMenu(
+                [Question::class, 'create'],
+                [Button::class, 'create']
+            )
+        )->asQuestion()
+    );
+});
+$botman->hears('auth', VetmanagerController::class.'@authConversation');
+$botman->hears('timesheet', VetmanagerController::class.'@timesheetConversation');
+$botman->hears('admissions', VetmanagerController::class.'@admissionConversation');
+//$botman->
+
+/** Examples
+
 $botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$bot->reply('Hello!');
 });
 
 $botman->hears('Hello', function ($bot) {
-    $userStorage = $bot->userStorage($bot->getUser());
-    $bot->reply($userStorage->get('token'));
+$userStorage = $bot->userStorage($bot->getUser());
+$bot->reply($userStorage->get('token'));
 });
 
 $botman->hears('call me {name}', function ($bot, $name) {
-    $bot->typesAndWaits(2);
-    $bot->userStorage()->save([
-        'name' => $name
-    ]);
+$bot->typesAndWaits(2);
+$bot->userStorage()->save([
+'name' => $name
+]);
 
-    $bot->reply('I will call you '.$name);
+$bot->reply('I will call you '.$name);
 });
 
 
 $botman->hears("who am I", function (BotMan $bot) {
-    // Retrieve information for the currently logged in user.
-    // You can also pass a user-id / key as a second parameter.
-    $user = $bot->userStorage($bot->getUser());
-    if ($name = $user->get('name')) {
-        $bot->reply('You are ' . $name);
-    } else {
-        $bot->reply('I do not know you yet.');
-    }
+// Retrieve information for the currently logged in user.
+// You can also pass a user-id / key as a second parameter.
+$user = $bot->userStorage($bot->getUser());
+if ($name = $user->get('name')) {
+$bot->reply('You are ' . $name);
+} else {
+$bot->reply('I do not know you yet.');
+}
 });
 
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
-
-$botman->hears('start', VetmanagerController::class.'@authConversation');
-
-$botman->hears('timesheet', VetmanagerController::class.'@timesheetConversation');
-
-$botman->hears('My Admission', VetmanagerController::class.'@admissionConversation');
+ *
+ */
---
+++ tests/Unit/Vetmanager/MainMenuTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\Vetmanager;
+
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use Tests\TestCase;
+use App\Vetmanager\MainMenu;
+
+class MainMenuTest extends TestCase
+{
+    public function testMainMenuReturnIsAQuestion(): void
+    {
+        $this->assertInstanceOf(
+            Question::class,
+            (
+                new MainMenu(
+                    [Question::class, 'create'],
+                    [Button::class, 'create']
+                )
+            )->asQuestion()
+        );
+    }
+
+    public function testMainMenuExistAuthAction(): void
+    {
+        $question = (
+            new MainMenu(
+                [Question::class, 'create'],
+                [Button::class, 'create']
+            )
+        )->asQuestion();
+        $this->assertTrue(
+            str_contains(json_encode($question->toArray()), 'auth')
+        );
+    }
+}
