df8a9a3ce94706077cd935259853e9340b57cb68
Danil Sirota <danilsirotalol@gmail.com>
2021-02-09 13:08:56 +0200

    close #12

---
+++ app/Http/Middleware/BotMan/ReceivedMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Http\Middleware\BotMan;
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Interfaces\Middleware\Captured;
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+use Illuminate\Support\Facades\DB;
+
+class ReceivedMiddleware implements Captured
+{
+    /**
+     * Handle an incoming message.
+     *
+     * @param IncomingMessage $message
+     * @param callable $next
+     * @param BotMan $bot
+     *
+     * @return mixed
+     */
+    public function captured(IncomingMessage $message, $next, BotMan $bot)
+    {
+        DB::table('statistic')->insert([
+            'created_at' => date("Y-m-d H:i:s"),
+            'user_id' => $message->getSender(),
+            'channel' => $bot->getDriver()->getName(),
+            'event' => 'incoming message'
+        ]);
+
+        return $next($message);
+    }
+}
