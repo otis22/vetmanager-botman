77ffc328c04114dde256dffce186583d05d72dce
Vladimir ROmanichev <pro100print@yahoo.com>
2021-02-10 13:10:59 +0300

    fix db config

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
         ],
 
         'pgsql' => [
