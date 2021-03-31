02356a9a8af5ee0abae0c3df12b44074a1c30c01
Danil Sirota <danilsirotalol@gmail.com>
2021-03-31 14:48:38 +0300

    schedules failfast refactoring

--- app/Vetmanager/MessageBuilder/Timesheet/TimesheetMessageBuilder.php
+++ app/Vetmanager/MessageBuilder/Timesheet/TimesheetMessageBuilder.php
@@ -40,17 +40,13 @@ class TimesheetMessageBuilder implements MessageBuilderInterface
     public function buildMessage()
     {
         $message = "";
-        if (!empty($this->timesheets)) {
-            foreach ($this->timesheets as $timesheet) {
-                $date = date_format(date_create($timesheet['begin_datetime']), "d.m.Y");
-                $from = date_format(date_create($timesheet['begin_datetime']), "H:i:s");
-                $to = date_format(date_create($timesheet['end_datetime']), "H:i:s");
-                $type = $this->api->getTypeNameById($timesheet['type']);
-                $message .= $date . PHP_EOL ."$from - $to" . PHP_EOL . $type;
-                $message .= PHP_EOL . PHP_EOL;
-            }
-        } else {
-            $message = "У вас нет рабочих графиков.";
+        foreach ($this->timesheets as $timesheet) {
+            $date = date_format(date_create($timesheet['begin_datetime']), "d.m.Y");
+            $from = date_format(date_create($timesheet['begin_datetime']), "H:i:s");
+            $to = date_format(date_create($timesheet['end_datetime']), "H:i:s");
+            $type = $this->api->getTypeNameById($timesheet['type']);
+            $message .= $date . PHP_EOL ."$from - $to" . PHP_EOL . $type;
+            $message .= PHP_EOL . PHP_EOL;
         }
         return $message;
     }
