22cafb217483a75d01cbab3b8fac23179cda6376
Danil Sirota <danilsirotalol@gmail.com>
2021-04-08 12:13:26 +0300

    close #73

--- app/Http/Middleware/BotMan/HeardMiddleware.php
+++ app/Http/Middleware/BotMan/HeardMiddleware.php
@@ -5,13 +5,14 @@ namespace App\Http\Middleware\BotMan;
 
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\Interfaces\Middleware\Captured;
+use BotMan\BotMan\Interfaces\Middleware\Heard;
 use BotMan\BotMan\Messages\Incoming\IncomingMessage;
 use Illuminate\Support\Facades\DB;
 
-class CapturedMiddleware implements Captured
+class HeardMiddleware implements Heard
 {
     /**
-     * Handle an incoming message.
+     * Handle a message that was successfully heard, but not processed yet.
      *
      * @param IncomingMessage $message
      * @param callable $next
@@ -19,13 +20,14 @@ class CapturedMiddleware implements Captured
      *
      * @return mixed
      */
-    public function captured(IncomingMessage $message, $next, BotMan $bot)
+    public function heard(IncomingMessage $message, $next, BotMan $bot)
     {
         DB::table('statistic')->insert([
             'created_at' => date("Y-m-d H:i:s"),
             'user_id' => $message->getSender(),
             'channel' => $bot->getDriver()->getName(),
-            'event' => 'incoming message'
+            'event' => 'incoming message',
+            'message' => $message->getText()
         ]);
 
         return $next($message);
